3068e84aab8583bc155c871b021a10abba7f7438
Dawid Kubicki <dawid@pluscode.io>
2021-03-15 10:45:47 +0100

    TabView added

---
+++ Africa/Africa/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Dawid Kubicki on 15/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Content")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
