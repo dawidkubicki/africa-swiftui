df5bca57fdce7b54cd88c3dec76c544ba9e8b104
Dawid Kubicki <dawid@pluscode.io>
2021-03-16 15:06:22 +0100

    minor changes

---
+++ Africa/Africa/View/CoverImageView.swift
@@ -0,0 +1,29 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Dawid Kubicki on 16/03/2021.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    
+    //MARK: - Properties
+    
+    //MARK: - Body
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+//MARK: - Preview
+
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}
